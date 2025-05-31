cb5b1b09f310fd88baccb9fe8983ad1d3ba09a4e
LizaGiorgobiani <150850677+LizaGiorgobiani@users.noreply.github.com>
2025-05-31 15:22:42 +0400

    Revert "opportunity-type-api"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
-import { getOpportunities } from "./services/api";
-import type { Opportunity } from "./types/Opportunity";
-
 function App() {
-  const [data, setData] = useState<Opportunity[]>([]);
-
-  useEffect(() => {
-    getOpportunities().then(setData).catch(console.error);
-  }, []);
-
   return (
     <div>
-      <h1>Volunteer Opportunities</h1>
-      <ul>
-        {data.map((opp) => (
-          <li key={opp.id}>
-            {opp.title} - {opp.location}
-          </li>
-        ))}
-      </ul>
+      <h1>Volunteer Opportunities App</h1>
     </div>
   );
 }
--- client/src/types/Opportunity.ts
+++
@@ -1,8 +0,0 @@
-export interface Opportunity {
-  id: string;
-  title: string;
-  description: string;
-  location: string;
-  date: string;
-  type: string;
-}
