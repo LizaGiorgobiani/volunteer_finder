76b54f965b20708db119601c8379322a08c6b1e9
LizaGiorgobiani <lizamg2005@gmail.com>
2025-06-08 20:25:00 +0400

    search bar

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Props = {
+  keyword: string;
+  setKeyword: (value: string) => void;
+};
+
+const SearchBar: React.FC<Props> = ({ keyword, setKeyword }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Search opportunities..."
+      value={keyword}
+      onChange={(e) => setKeyword(e.target.value)}
+      className="w-full p-2 border rounded"
+    />
+  );
+};
+
+export default SearchBar;
